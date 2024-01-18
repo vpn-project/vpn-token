0c6396c91db51416e3b15de6eb5a085379de3dd0
nesterrovv <nesterov.iv2018@gmail.com>
2024-01-18 13:23:18 +0700

    fix: codestyle problems

--- src/main/java/com/nesterrovv/vpntoken/configuration/JwtFilter.java
+++ src/main/java/com/nesterrovv/vpntoken/configuration/JwtFilter.java
@@ -5,20 +5,21 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
-import java.io.IOException;
-import java.util.List;
 
 public class JwtFilter extends OncePerRequestFilter {
 
-    private final String BEARER = "Bearer";
+    private static final String BEARER = "Bearer";
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+        throws ServletException, IOException {
         String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
         if (authHeader == null || !authHeader.startsWith(BEARER)) {
             filterChain.doFilter(request, response);

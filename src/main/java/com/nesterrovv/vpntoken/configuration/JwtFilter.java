cb3fe0e4bce0dced2a48e099d78096b505efaa18
Kirill <kirill.vorobyev2002@yandex.ru>
2024-01-18 05:59:33 +0500

    feat(auth): added authorization for this ms

---
+++ src/main/java/com/nesterrovv/vpntoken/configuration/JwtFilter.java
@@ -0,0 +1,34 @@
+package com.nesterrovv.vpntoken.configuration;
+
+import com.nesterrovv.vpntoken.entity.Role;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
+import java.util.List;
+
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final String BEARER = "Bearer";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (authHeader == null || !authHeader.startsWith(BEARER)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        String username = request.getHeader("username");
+        Role userRole = Role.valueOf(request.getHeader("authorities"));
+        Authentication authentication = new UsernamePasswordAuthenticationToken(username, null, List.of(userRole));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        filterChain.doFilter(request, response);
+    }
+}
+
